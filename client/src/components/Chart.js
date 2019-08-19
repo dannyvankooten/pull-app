a3af5cbd8f1ae32347f9d655e3414c2ec38bb23d
Danny van Kooten <dannyvankooten@gmail.com>
2019-08-19 13:44:56 +0200

    add bar chart to stats page

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,9 +1,79 @@
 import React from 'react';
 import './Chart.css';
+const monthNames = [
+	"Jan", "Feb", "Mar",
+	"Apr", "May", "Jun", "Jul",
+	"Aug", "Sep", "Oct",
+	"Nov", "Dec"
+];
 
 class Chart extends React.Component {
 	render() {
-		return '';
+		let now = new Date();
+		let data = this.props.data.map(d => {
+			d.date = new Date(d.date);
+			return d;
+		});
+		let max = 0;
+		let bars = [];
+
+		let weekStart = new Date();
+		weekStart.setDate(now.getDate() - 90);
+		weekStart.setDate(weekStart.getDate() + (0-weekStart.getDay())); // set to Sunday
+		while(weekStart <= now) {
+			let weekEnd = new Date(weekStart);
+			weekEnd.setDate(weekStart.getDate() + 7);
+			let total = 0;
+
+			data.forEach(d => {
+				if (d.date >= weekStart && d.date < weekEnd) {
+					total += d.total
+				}
+			});
+
+			if (total > max) {
+				max = total;
+			}
+			bars.push({ x: weekStart, y: total });
+			weekStart = weekEnd
+		}
+
+		max = Math.ceil(max / 10) * 10;
+		max = Math.max(10, max);
+		return (
+			<div className={"chart"}>
+				<div className="y-axis">
+					<div style={{bottom: '100%'}}>{max}</div>
+					<div style={{bottom: '50%'}}>{max/2}</div>
+					<div style={{bottom: '0%'}}>0</div>
+				</div>
+				<div className="bars">
+					{bars.map((b, i) => {
+						return (<div className="interval" key={i} style={{
+							left: (i / bars.length*100) + '%',
+							width: (100 / bars.length) + '%'
+						}}>
+							<div className={"bar"}  style={{
+								height: parseInt(b.y / max * 100) + '%',
+							}}>
+								<span>{b.y}</span>
+							</div>
+						</div>)
+					})}
+				</div>
+				<div className={"x-axis"}>
+					{bars.map((b, i) => {
+						return (
+							<div key={i} style={{
+								left: (i / bars.length*100) + '%',
+							}}>
+								<div className="tick" />
+								<span className="label">{b.x.getDate() <= 7 ? monthNames[b.x.getMonth()] : ''}</span>
+							</div>)
+					})}
+				</div>
+			</div>
+		)
 	}
 }
 
--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -19,7 +19,45 @@ CREATE TABLE activities (
 INSERT INTO users(username, password) VALUES('danny', '$2b$10$MA89pFh3ej6ZjA35muLjvejuDgF9XwVJcj2k/31GagDw4DNp/28yi');
 INSERT INTO users(username, password) VALUES('johndoe', '$2b$10$MA89pFh3ej6ZjA35muLjvejuDgF9XwVJcj2k/31GagDw4DNp/28yi');
 
-INSERT INTO activities(user_id, repetitions, timestamp) VALUES (1, 4, datetime('now', '-30 days')), (1, 10, datetime('now', '-30 days')), (1, 3, datetime('now', '-7 day')), (1, 4, datetime('now', '-2 days')), (1, 5, datetime('now', '-2 days')), (1, 5, datetime('now', '-2 days')), (1, 5, datetime('now', '-1 day')), (1, 5, datetime('now', '-1 day')), (1, 6, datetime('now', '-3 minutes')), (1, 6, datetime('now', '-2 minutes')), (1, 6, datetime('now', '-1 minute'));
+INSERT INTO activities(user_id, repetitions, timestamp) VALUES (1, 3, datetime('now', '-50 days')),
+                                                               (1, 5, datetime('now', '-50 days')),
+                                                               (1, 7, datetime('now', '-50 days')),
+                                                               (1, 3, datetime('now', '-45 days')),
+                                                               (1, 2, datetime('now', '-45 days')),
+                                                               (1, 1, datetime('now', '-45 days')),
+                                                               (1, 3, datetime('now', '-41 days')),
+                                                               (1, 3, datetime('now', '-41 days')),
+                                                               (1, 4, datetime('now', '-41 days')),
+                                                               (1, 5, datetime('now', '-41 days')),
+                                                               (1, 7, datetime('now', '-36 days')),
+                                                               (1, 7, datetime('now', '-36 days')),
+                                                               (1, 4, datetime('now', '-32 days')),
+                                                               (1, 4, datetime('now', '-32 days')),
+                                                               (1, 4, datetime('now', '-31 days')),
+                                                               (1, 4, datetime('now', '-31 days')),
+                                                               (1, 4, datetime('now', '-31 days')),
+                                                               (1, 3, datetime('now', '-31 days')),
+                                                               (1, 4, datetime('now', '-30 days')),
+                                                               (1, 4, datetime('now', '-30 days')),
+                                                               (1, 4, datetime('now', '-30 days')),
+                                                               (1, 4, datetime('now', '-30 days')),
+                                                               (1, 2, datetime('now', '-25 days')),
+                                                               (1, 8, datetime('now', '-20 days')),
+                                                               (1, 9, datetime('now', '-20 days')),
+                                                               (1, 3, datetime('now', '-15 days')),
+                                                               (1, 3, datetime('now', '-15 days')),
+                                                               (1, 12, datetime('now', '-10 days')),
+                                                               (1, 10, datetime('now', '-10 days')),
+                                                               (1, 10, datetime('now', '-8 days')),
+                                                               (1, 3, datetime('now', '-7 day')),
+                                                               (1, 4, datetime('now', '-2 days')),
+                                                               (1, 5, datetime('now', '-2 days')),
+                                                               (1, 5, datetime('now', '-2 days')),
+                                                               (1, 5, datetime('now', '-1 day')),
+                                                               (1, 5, datetime('now', '-1 day')),
+                                                               (1, 6, datetime('now', '-3 minutes')),
+                                                               (1, 6, datetime('now', '-2 minutes')),
+                                                               (1, 6, datetime('now', '-1 minute'));
 
 -- Down
 DROP TABLE users;
