eef3cfc37856a73307d27fba24003438f173e065
Danny van Kooten <dannyvankooten@gmail.com>
2019-09-07 20:34:24 +0200

    add year view to chart

--- client/components/Chart.js
+++ client/components/Chart.js
@@ -4,10 +4,10 @@ import {
     StyleSheet,
 	Text
 } from 'react-native';
-import { VictoryChart, VictoryTheme, VictoryBar, VictoryAxis, VictoryTooltip, VictoryLabel } from "victory-native";
+import { VictoryChart, VictoryTheme, VictoryBar, VictoryAxis } from "victory-native";
 
 const days = [ 'S', 'M', 'T', 'W', 'T', 'F', 'S' ];
-const months = [ 'J', 'F', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'];
+const months = [ 'J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'];
 const monthNames = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 export default function Chart(props) {
@@ -20,26 +20,28 @@ export default function Chart(props) {
         });
         let barData = [];
         const period = props.period || 'week';
-        const numberOfBars = period === 'week' ? 7 : 12;
+        const numberOfBars = {week: 7, month: 5, year: 12}[period];
 
         for(let i = numberOfBars; i > 0; i--) {
             let iStart = new Date();
-            iStart.setHours(0);
-            iStart.setMinutes(0);
+            iStart.setHours(0, 0, 0);
 
-            if (period === 'month') {
-                iStart.setMonth(now.getMonth() - 1);
-                iStart.setDate(1);
-            } else {
-                iStart.setDate(now.getDate() + (8 - iStart.getDay()) + (props.periodAgo * 7)); // set to Monday
-                iStart.setDate(iStart.getDate() - i);
+            if (period === 'week') {
+				iStart.setDate(now.getDate() + (8 - iStart.getDay()) + (props.periodAgo * 7) - i);
+            } else if (period === 'month') {
+				iStart.setDate(now.getDate() + (8 - iStart.getDay()) + (props.periodAgo * 7 * 5) - i * 7);
+			} else if (period === 'year') {
+				iStart.setDate(1);
+				iStart.setMonth(now.getMonth() + (12 - now.getMonth()) + ( props.periodAgo * 12 ) - i);
             }
 
             let iEnd = new Date(iStart);
-            if (period === 'month') {
-                iEnd.setMonth(iStart.getMonth() + 1);
-            } else {
-                iEnd.setDate(iStart.getDate() + 1);
+            if (period === 'week') {
+				iEnd.setDate(iStart.getDate() + 1);
+			} else if(period === 'month') {
+            	iEnd.setDate(iStart.getDate() + 7);
+            } else if(period === 'year') {
+				iEnd.setMonth(iStart.getMonth() + 1);
             }
 
             let total = 0;
@@ -57,6 +59,19 @@ export default function Chart(props) {
         const startDate = barData[0].x;
         const endDate = barData[barData.length-1].x;
 
+        const tickFormatters = {
+        	week: d => days[d.getDay()],
+			month: d => '',
+			year: d => months[d.getMonth()],
+		};
+
+        let subtitle;
+        if (period === 'week') {
+			subtitle = `${monthNames[startDate.getMonth()]} ${startDate.getDate()} - ${monthNames[endDate.getMonth()]} ${endDate.getDate()}`
+		} else {
+			subtitle = `${monthNames[startDate.getMonth()]} ${startDate.getDate()} - ${monthNames[endDate.getMonth()]} ${endDate.getDate()}, ${startDate.getFullYear()}`
+		}
+
         return (
             <View>
                 <VictoryChart
@@ -68,10 +83,10 @@ export default function Chart(props) {
                     height={200}
                     padding={{ top: 20, bottom: 40, left: 0, right: 40 }}
                     domainPadding={10}>
-                    <VictoryAxis tickValues={barData.map(v => v.x)} tickFormat={d => days[d.getDay()]} />
+                    <VictoryAxis tickValues={barData.map(v => v.x)} tickFormat={tickFormatters[period]} />
                     <VictoryBar data={barData} labels={({ datum }) => datum.y > 0 ? String(Math.round(datum.y)) : ''} />
                 </VictoryChart>
-				<Text style={styles.muted}>{monthNames[startDate.getMonth()]} {startDate.getDate()} - {monthNames[endDate.getMonth()]} {endDate.getDate()}</Text>
+				<Text style={styles.muted}>{subtitle}</Text>
             </View>
         )
 }
